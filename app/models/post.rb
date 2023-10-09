1c7cd3d7c0eb7fa402bc663e0e04fa9dbf062be0
rextumlos <johnrex412@gmail.com>
2023-10-09 14:57:18 +0800

    Post: Created post table with reference to user and category

---
+++ app/models/post.rb
@@ -0,0 +1,9 @@
+class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :address, presence: true
+
+  has_many :post_category_ships
+  has_many :categories, through: :post_category_ships
+  belongs_to :user
+end
