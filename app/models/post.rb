e57f6b78b81720736f35507a32bb9e0bfc0ed41c
rextumlos <johnrex412@gmail.com>
2023-10-09 15:04:46 +0800

    Comment: Created table and added ref to post and user

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ApplicationRecord
   validates :content, presence: true
   validates :address, presence: true
 
+  has_many :comments
   has_many :post_category_ships
   has_many :categories, through: :post_category_ships
   belongs_to :user
