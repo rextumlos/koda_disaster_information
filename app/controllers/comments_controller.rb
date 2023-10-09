4f5787e57171d1bb27f2b12ab5dd25ec52bd31c5
rextumlos <johnrex412@gmail.com>
2023-10-09 20:17:13 +0800

    Comment: Added user's comments for user/comments route

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,24 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
   def index
+    @comments = current_user.comments.includes(:post)
   end
 
   def new
   end
 
+  def create
+
+  end
+
   def edit
+  end
+
+  def update
+
+  end
+
+  def destroy
+
   end
 end
