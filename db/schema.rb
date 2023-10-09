8559676e251019a7b7778495481807842e2c6c04
rextumlos <johnrex412@gmail.com>
2023-10-09 14:58:18 +0800

    PostCategoryShip: Created PostCategoryShip

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_09_060918) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_09_063905) do
+  create_table "categories", charset: "utf8mb4", force: :cascade do |t|
+    t.string "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "post_category_ships", charset: "utf8mb4", force: :cascade do |t|
+    t.bigint "post_id"
+    t.bigint "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_post_category_ships_on_category_id"
+    t.index ["post_id"], name: "index_post_category_ships_on_post_id"
+  end
+
+  create_table "posts", charset: "utf8mb4", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "title"
+    t.text "content"
+    t.string "address"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -23,4 +49,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_09_060918) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
