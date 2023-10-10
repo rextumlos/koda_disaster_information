8c844ebebbb1a7d6ceb6c428ad4bdb1eaa768d86
rextumlos <johnrex412@gmail.com>
2023-10-10 10:31:27 +0800

    Comment: Debug user/comment can't display user's comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_post
+  before_action :set_post, except: :index
   before_action :set_comment, only: [:edit, :update, :destroy]
   def index
     @comments = current_user.comments.includes(:post)
