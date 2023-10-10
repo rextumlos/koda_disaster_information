b01df09e708a378a22424bb0d336127b0bdc75e0
rextumlos <johnrex412@gmail.com>
2023-10-10 10:27:31 +0800

    Post: Added comment counter feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_09_070113) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_10_022013) do
   create_table "categories", charset: "utf8mb4", force: :cascade do |t|
     t.string "category"
     t.datetime "created_at", null: false
@@ -44,6 +44,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_09_070113) do
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "comments_count"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
