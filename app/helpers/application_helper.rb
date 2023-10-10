7e75f9d59383b77eaea241ef659fe8aefbf1cabc
rextumlos <johnrex412@gmail.com>
2023-10-10 18:10:04 +0800

    Flash: Debug flash_class in application.helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
   def flash_class(level)
     bootstrap_alert_class = {
-      success: "alert-class",
-      error: "alert-danger",
-      notice: "alert-info",
-      alert: "alert-danger",
-      warn: "alert-warning"
+      "success" => "alert-success",
+      "error" => "alert-danger",
+      "notice" => "alert-info",
+      "alert" => "alert-danger",
+      "warn" => "alert-warning"
     }
 
     bootstrap_alert_class[level]
