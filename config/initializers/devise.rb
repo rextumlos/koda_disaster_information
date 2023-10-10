b0be1a4e600c472477300afd29a5adce2d36649f
rextumlos <johnrex412@gmail.com>
2023-10-10 18:05:37 +0800

    User: Added style in login and signup page

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -312,4 +312,5 @@ Devise.setup do |config|
   # config.sign_in_after_change_password = true
   #
   config.navigational_formats = ['*/*', :html, :turbo_stream]
+  config.scoped_views = true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  devise_scope :users do
+    get 'sign_in', to: 'users/sessions#new'
+  end
+
   # Defines the root path route ("/")
   root "welcome#index"
 
