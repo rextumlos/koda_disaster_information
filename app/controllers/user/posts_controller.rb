9cd1dfd0ca6ec688a414dee5df484200a449c2da
rextumlos <johnrex412@gmail.com>
2023-10-11 11:46:03 +0800

    Add: Authenticate user in user/posts and user/comments

--- app/controllers/user/posts_controller.rb
+++ app/controllers/user/posts_controller.rb
@@ -1,4 +1,5 @@
 class User::PostsController < ApplicationController
+  before_action authenticate_user!
   def index
     @posts = current_user.posts.includes(:categories)
                          .order(comments_count: :desc)
