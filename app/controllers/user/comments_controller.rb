e6bc09791fb631f4520c8c12bc4498019441da33
rextumlos <johnrex412@gmail.com>
2023-10-11 19:55:20 +0800

    Fix: :auth in user/posts & user/comments controller

--- app/controllers/user/comments_controller.rb
+++ app/controllers/user/comments_controller.rb
@@ -1,5 +1,5 @@
 class User::CommentsController < ApplicationController
-  before_action authenticate_user!
+  before_action :authenticate_user!
   def index
     @comments = current_user.comments.includes(:post)
                             .order(created_at: :desc)
